9fdae1ee4eba74991a525eded318954c1c353832
Sven Primus <svenprimus@gmail.com>
2026-07-13 20:01:47 +0200

    feat: render cards

---
+++ script/templates-card.js
@@ -0,0 +1,116 @@
+function getProfile(index) {
+    return /*html*/ `
+        <article id="profile-card-${index}" class="profile-card">
+            <div class="profile-card-content">
+                <h2>${mentors[index].name}</h2>
+            </div>
+            <hr />
+            ${getProfileContent(index)}
+        </article>
+    `;
+}
+
+function getProfileContent(index) {
+    return /*html*/`
+        ${getSectionPicture(index)}
+        <hr />
+        ${getSectionPricingDetails(index)}
+        <hr />
+        ${getSectionBlurb(index)}
+        <hr />
+        ${getSectionComments(index)}
+    `;
+}
+function getSectionPicture(index) {
+    return /*html*/ `
+        <div class="profile-card-content">
+            <img
+                aria-haspopup="dialog"
+                aria-controls="pictureDialog"
+                onclick="openDialog(${index})"
+                class="profile-picture"
+                src="${mentors[index].image}"
+                alt="${mentors[index].alt}"
+                tabindex="0"
+            />
+        </div>
+    `;
+}
+
+function getSectionPricingDetails(index) {
+    return /*html*/ `
+        <section class="profile-card-content">
+            <section class="section-pricing">
+                ${getPricingAndLikes(index)}
+            </section>
+            <section class="section-details">
+                ${getDetailsTable(index)}
+            </section>
+        </section>
+    `;
+}
+
+function getPricingAndLikes(index) {
+    return /*html*/ `
+        <p>${mentors[index].price} €</p>
+        <div>
+            <p>${mentors[index].likes}</p>
+            <button id="button-favorite-${index}" class="button-favorite"
+                aria-description="add mentor to favorites">
+            </button>
+        </div>
+    `;
+}
+
+function getDetailsTable(index) {
+    return /*html*/ `
+        <table>
+            <tr>
+                <th>Saga</th>
+                <td>: <span title="${mentors[index].saga}">${mentors[index].saga}</span></td>
+            </tr>
+            <tr>
+                <th>Published</th>
+                <td>: ${mentors[index].publishedYear}</td>
+            </tr>
+            <tr>
+                <th>Genre</th>
+                <td>: ${mentors[index].genre}</td>
+            </tr>
+        </table>
+    `;
+}
+
+function getSectionBlurb(index) {
+    return /*html*/ `
+        <section class="section-blurb profile-card-content">
+            <p aria-haspopup="dialog" aria-controls="pictureDialog" onclick="openDialog(${index})" tabindex="0">
+                ${mentors[index].blurb}
+            </p>
+        </section>
+    `;
+}
+
+function getSectionComments(index) {
+    return /*html*/ `
+        <section class="section-comments profile-card-content">
+            <h3>Comments</h3>
+            <div class="section-comments-table">
+                <table id="comments-table-${index}"></table>
+            </div>
+            <div class="section-comments-input">
+                <input type="text" name="new-comment" />
+                <button class="button-send" aria-description="send comment"></button>
+            </div>
+        </section>
+    `;
+}
+
+function getCommentRow(indexMentor, indexComment) {
+    return /*html*/ `
+        <tr>
+            <th>${mentors[indexMentor].comments[indexComment].name}</th>
+            <td>: ${mentors[indexMentor].comments[indexComment].comment}</td>
+        </tr>
+    `;
+}
---
+++ script/templates-dialog.js
@@ -0,0 +1,66 @@
+function getDialogContent(index, label) {
+    return /*html*/ `
+        <section aria-description="dialog window for bigger view of image" class="dialog-content-wrapper" 
+            onclick="stopDialogPropagation(event)">
+            ${getModalHeadWrapper(label)}
+            ${getModalPictureWrapper(index)}
+            ${getModalNavigationWrapper(index)}
+        </section>
+    `;
+}
+
+function getModalHeadWrapper(label) {
+    return /*html*/ `
+        <header id="modalHead" class="modal-head-close">
+            ${getModalHeadContent(label)}
+        </header>
+    `;
+}
+
+function getModalHeadContent(label) {
+    return /*html*/ `
+        <button id="dialogClose" class="close-button" 
+            aria-controls="pictureDialog" aria-label="close dialog" 
+            onclick="closeDialog()">
+        </button>
+        <h2 id="dialogTitle">${label}</h2>
+    `;
+}
+
+function getModalPictureWrapper(index) {
+    return /*html*/ `
+        <section id="dialogPicture" class="dialog-picture-wrapper">
+            ${getModalPictureContent(index)}
+        </section>
+    `;
+}
+
+function getModalPictureContent(index) {
+    return /*html*/ `
+        <img class="dialog-picture" src=${mentors[index].image} alt="${mentors[index].alt}"/>
+    `;
+}
+
+function getModalNavigationWrapper(index) {
+    return /*html*/ `
+        <nav id="modalNav" class="modal-nav">
+            ${getModalNavigationContent(index)}
+        </nav>      
+    `;
+}
+
+function getModalNavigationContent(index) {
+    return /*html*/ `
+        <button id="dialogPrevious" class="arrow-button-left" tabindex="0"
+            aria-controls="dialogPicture" aria-label="previous picture" aria-describedby="current image index"
+            onclick="renderPreviousImage(${index})">
+            <div aria-hidden="true"></div>
+        </button>
+        <p aria-label="current image index">${index + 1} / ${mentors.length}</p>
+        <button id="dialogNext" class="arrow-button-right" tabindex="0"
+            aria-controls="dialogPicture" aria-label="next picture" aria-describedby="current image index"
+            onclick="renderNextImage(${index})">
+            <div aria-hidden="true"></div>
+        </button>
+    `;
+}
